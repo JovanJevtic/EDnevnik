2f9f508225e377e453431925bbacdd4a8957ac88
Jovan Jevtic <jovanjevtic132@gmail.com>
2022-12-27 00:04:40 +0100

    material table working

--- client/src/pages/AdminPages/AdminPredmetiPage.tsx
+++ client/src/pages/AdminPages/AdminPredmetiPage.tsx
@@ -1,21 +1,40 @@
 import { IonContent, IonPage, IonTitle } from "@ionic/react";
 import Header from "../../components/Header";
 import { IAdmin } from "../../features/admin/authAdminSlice";
+import MaterialTable from 'material-table'
+
+import { ThemeProvider, createTheme } from '@mui/material';
+
+import CssBaseline from '@mui/material/CssBaseline'; 
 
 interface AdminPredmetiPageProps {
     admin: IAdmin;
     onLogout: () => void;
 }
 
+
+
 const AdminPredmetiPage: React.FC<AdminPredmetiPageProps> = ({ admin, onLogout }) => {
+    const darkTheme = createTheme({
+        palette: {
+          mode: 'dark',
+        },
+    });
+
     return (
         <IonPage>
             <Header ime={admin?.ime as string} onLogout={onLogout} userType="admin" />
-                <IonContent>
-                    <IonTitle>
-                        This is the fuuking predmeti page
-                    </IonTitle>
-                </IonContent>
+            <IonContent>
+                <ThemeProvider theme={darkTheme}>
+                    <CssBaseline>
+                        <MaterialTable
+                            columns={[{ title: 'Naziv Predmeta', field: 'name' }]}
+                            data={[{ name: 'Hemija' }, { name: 'Matematika' }, { name: 'Matematika' }, { name: 'Matematika' }, { name: 'Matematika' }, { name: 'Matematika' }, { name: 'Matematika' }, { name: 'Matematika' }]}
+                            title="Predmeti"
+                        />
+                    </CssBaseline>
+                </ThemeProvider>
+            </IonContent>
         </IonPage>
     );
 }
